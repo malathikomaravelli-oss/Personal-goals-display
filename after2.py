992b565d06c56311412ac3359a486906b39aed1c
malathikomaravelli-oss <malathi.komaravelli@gmail.com>
2026-09-20 19:37:11 +0530

    Add files via upload

---
+++ after2.py
@@ -0,0 +1,13 @@
+name = input("What is your name? ")
+goal = input("What is your main goal? ")
+steps = input("What will you do to achieve your goal? ")
+
+print("\n--- PERSONAL GOALS PLAN ---")
+print("Name:", name, "Goal:", goal)
+print("Steps:", steps)
+
+print("\nKeep working towards your goal!", end=" ")
+print("You can do it!")
+
+import keyword
+print(keyword.kwlist)
